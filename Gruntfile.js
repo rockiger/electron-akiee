85aa3d168dba8e0ae9b9c37e4d543584e1f4638d
Marco Laspe <marco@rockiger.com>
2017-06-16 14:53:09 +0200

    Update electron 1.6.11

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         "download-electron": {
-            version: "1.6.2",
+            version: "1.6.11",
             outputDir: "./electron",
             // if you want to download electron into project directory
             // downloadDir: ".electron-download",
